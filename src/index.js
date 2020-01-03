3101a35b71e1004f8d3dbaf0dcdecf266ff5cea5
Xin Zhang <xin_zhang_2000@yahoo.com>
2020-01-03 16:09:34 +1100

    add redux

--- src/index.js
+++ src/index.js
@@ -3,22 +3,12 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./reducers";
-
-import "./styles.css";
+import App from "./app";
 
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
